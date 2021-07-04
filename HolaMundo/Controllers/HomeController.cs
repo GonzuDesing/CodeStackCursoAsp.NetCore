bdbc97190a1db05b6d80cdba7f4134bcbd162aa2
GonzuDesing <gonzu.desing@gmail.com>
2021-07-03 21:53:07 -0600

    03 - Controladores: Devolver Archivos al Navegador [NetCore MVC]

--- HolaMundo/Controllers/HomeController.cs
+++ HolaMundo/Controllers/HomeController.cs
@@ -6,36 +6,102 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HolaMundo.Models;
-
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 namespace HolaMundo.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //Creamos variable privada de la interfaz IWebHostEnvironment
+        //Variable de ambiente _env
 
-        public HomeController(ILogger<HomeController> logger)
+
+
+
+        private readonly IWebHostEnvironment env;
+
+
+        public HomeController(IWebHostEnvironment _env)
         {
-            _logger = logger;
+
+            env = _env;
         }
 
 
-//IActionResult devuelve cualquier tipo de dato
+
+
+
+        /*
+                private readonly ILogger<HomeController> _logger;
+                public HomeController(ILogger<HomeController> logger)
+                {
+                    _logger = logger;
+                }
+        */
+
+        /*
+        //SI SE TIENEN DOS CONTROLADORES IGUALES SE DEBE HACER ASI
+               private IWebHostEnvironment _env;
+               private readonly ILogger<HomeController> _logger;
+
+               //creamos el constructor
+
+               public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env)
+               {
+                   _logger = logger;
+                   _env = env;
+               }
+
+               */
+
+
+        //IActionResult devuelve cualquier tipo de dato
         public IActionResult Index()
         {
             //Puedo agregar el nombre de la vista a regresar
             return View();
         }
 
-//Los metodos pueden devolver vistas pero no siempre lo hacen
+
+
+
+
+        public IActionResult PDF()
+        {
+
+            string FilePath = Path.Combine(env.WebRootPath, "download/", "PruebaPDF.pdf");
+            string FilePath404 = Path.Combine(env.WebRootPath, "download/", "NoEncontrado.pdf");
+            //Validamos que el archivo exista
+            if (System.IO.File.Exists(FilePath))
+            {
+                //Lo instanciamos para poder utilizarlo
+                FileStream fs = new FileStream(FilePath, FileMode.Open);
+                //con octed-stream descarga el archivo
+                //el segundo parametro va ser el nombre del archivo al descargarse
+            
+                return File(fs,"application/octet-stream", "Archivo.pdf");
+                
+            }
+            else
+            {
+                //Lo instanciamos para poder utilizarlo
+               return Content("Archivo no encontrado");
+            }
+        }
+
+
+
+
+        //Los metodos pueden devolver vistas pero no siempre lo hacen
         public string HolaMundo(string Nombre)
         {
 
-            return "Hola "+Nombre;
+            return "Hola " + Nombre;
 
 
         }
 
-//AL LLAMRLO EN EL URL DEBO PONER    /Home/nombre del metodo  /Home/Acerca asi me llama la vista acerca
+        //AL LLAMRLO EN EL URL DEBO PONER    /Home/nombre del metodo  /Home/Acerca asi me llama la vista acerca
         public ViewResult Acerca()
         {
             return View();
